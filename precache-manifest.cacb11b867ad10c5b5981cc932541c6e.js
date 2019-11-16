ad4db691ba0e05b5e5b783e2d38896eece1298bf
akhil117 <akhil117>
2019-11-16 14:07:56 +0530

    Updates

---
+++ precache-manifest.cacb11b867ad10c5b5981cc932541c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9708aaf5730f7909b163dccec4ac8f",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "73e492ba9b1a00d3ad3b",
+    "url": "/MonstersRolodex/static/css/main.75a72534.chunk.css"
+  },
+  {
+    "revision": "081b479aaabafd2c6f62",
+    "url": "/MonstersRolodex/static/js/2.930bee93.chunk.js"
+  },
+  {
+    "revision": "73e492ba9b1a00d3ad3b",
+    "url": "/MonstersRolodex/static/js/main.9bfcf6d1.chunk.js"
+  },
+  {
+    "revision": "349f94552ebba927b647",
+    "url": "/MonstersRolodex/static/js/runtime-main.be184a18.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/MonstersRolodex/static/media/logo.25bf045c.svg"
+  }
+]);
